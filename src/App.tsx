13d0d2e6d7263d62ee8363aa36ce30a0d03730c0
Dawit Nigussie <dawit9n@gmail.com>
2026-01-17 11:35:14 +0300

    Add classroom structure with dashboard and subjects management pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,91 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { Refine } from "@refinedev/core";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
 
+import Dashboard from "./pages/Dashboard";
+import { BookOpen, Home } from "lucide-react";
+import { Layout } from "./components/refine-ui/layout/layout";
+import SubjectList from "./pages/subjects/List";
+import SubjectCreate from "./pages/subjects/Create";
+
 function App() {
+  const customTitleHandler = ({ resource, action }: any) => {
+    let title = "";
+
+    if (resource?.name) {
+      title = resource.meta?.label || resource.name;
+      if (action) {
+        title += ` | Classroom`;
+      }
+    }
+    return title;
+  };
+
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
-          <DevtoolsProvider>
-            <Refine
-              dataProvider={dataProvider}
-              notificationProvider={useNotificationProvider()}
-              routerProvider={routerProvider}
-              options={{
-                syncWithLocation: true,
-                warnWhenUnsavedChanges: true,
-                projectId: "1pRISa-2HyOvZ-RJNgsj",
-              }}
-            >
-              <Routes>
-                <Route index element={<WelcomePage />} />
-              </Routes>
-              <Toaster />
-              <RefineKbar />
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
-            </Refine>
-            <DevtoolsPanel />
-          </DevtoolsProvider>
+          <Refine
+            dataProvider={dataProvider}
+            notificationProvider={useNotificationProvider()}
+            routerProvider={routerProvider}
+            options={{
+              title: { text: "Classroom" },
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              projectId: "1pRISa-2HyOvZ-RJNgsj",
+            }}
+            resources={[
+              {
+                name: "dashboard",
+                list: "/",
+                meta: {
+                  label: "Home",
+                  icon: <Home />,
+                },
+              },
+              {
+                name: "subjects",
+                list: "/subjects",
+                create: "/subjects/create",
+                meta: {
+                  label: "Subjects",
+                  icon: <BookOpen />,
+                },
+              },
+            ]}
+          >
+            <Routes>
+              <Route
+                element={
+                  <Layout>
+                    <Outlet />
+                  </Layout>
+                }
+              >
+                <Route path="/" element={<Dashboard />} />
+
+                <Route path="subjects">
+                  <Route index element={<SubjectList />} />
+                  <Route path="create" element={<SubjectCreate />} />
+                </Route>
+              </Route>
+            </Routes>
+            <Toaster />
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler handler={customTitleHandler} />
+          </Refine>
         </ThemeProvider>
       </RefineKbarProvider>
     </BrowserRouter>
